aa2b4e44d4327b5fc2b01d5c4b6b62a1feb01319
Norris Oduro <norrisjibril@gmail.com>
2017-03-20 15:53:49 +0000

    add get and post methods to router

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 
 class Router{
 private:
-  std::map<std::string,std::function<void(std::string)>> m_routes;
+  std::map<std::string,std::function<void(std::string)>> get_routes;
+  std::map<std::string,std::function<void(std::string)>> post_routes;
+  
 public:
   Router() = default;
   Router(const Router&) = delete;
@@ -12,18 +14,62 @@ public:
   Router& operator = (const Router&) = delete;
   Router& operator = (const Router&&) = delete;
   
-  template <typename Url,typename Callback>
-	void add(Url&& url,Callback&& callback)
-	{
-    	m_routes[url] = callback;
-	}
- void dispatch(const char* url)
+  template<typename Url,typename Callback>
+  void get(const Url& url,const Callback& callback)
   {
-    for(std::map<std::string,std::function<void(std::string)>>::iterator it= m_routes.begin(); it!=m_routes.end();it++)
+    get_routes[url] = callback;
+  }
+  
+  template<typename Url,typename Callback>
+  void post(const Url& url,const Callback& callback)
+  {
+    post_routes[url] = callback;
+  }
+ 
+  void mapGetRoutes(const char* url)
+  {
+  	for(std::map<std::string,std::function<void(std::string)>>::iterator it= get_routes.begin(); it!=get_routes.end();it++)
     {
      	if(it->first == url){
         	std::invoke(it->second,it->first);
         }
+    }
+  }
+  void mapPostRoutes(const char* url)
+  {
+  	for(std::map<std::string,std::function<void(std::string)>>::iterator it= post_routes.begin(); it!=post_routes.end();it++)
+    {
+     	if(it->first == url){
+        	std::invoke(it->second,it->first);
+        }
+    }
+  }
+  bool routeExists(const char*url)
+  {
+    for(std::map<std::string,std::function<void(std::string)>>::iterator it= post_routes.begin(); it!=post_routes.end();it++)
+    {
+     	if(it->first == url){
+        	return true;
+        }
+    }
+    for(std::map<std::string,std::function<void(std::string)>>::iterator it= get_routes.begin(); it!=get_routes.end();it++)
+    {
+     	if(it->first == url){
+        	return true;
+        }
+    }
+    return false;
+  }
+  
+  void dispatch(const char* url)
+  {
+   	if(routeExists(url))
+    {
+      mapGetRoutes(url);
+      mapPostRoutes(url);
+    }else
+    {
+      throw std::runtime_error(std::string("Route not found --- ").append(url));
     }
   }
   
@@ -31,12 +77,16 @@ public:
 
 struct HomeController{
 void getUserHome(){}
+void login(){}
 }homeController;
 
 int main([[maybe_unused]] int argc,[[maybe_unused]]const char* argv[])
 {
-	router.add("/home",[]([[maybe_unused]]std::string url){
+	router.get("/home",[&]([[maybe_unused]]std::string url){
       return std::invoke(&HomeController::getUserHome,homeController);
+    });
+  	router.post("/login",[&]([[maybe_unused]]std::string url){
+    	return std::invoke(&HomeController::login,homeController);
     });
    router.dispatch(argv[1]);
 }
